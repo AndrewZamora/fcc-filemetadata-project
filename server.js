fd0c01f34b184d94a16dec6ca464b713cba8bc53
AndrewZamora <andrewmichaelzamora@gmail.com>
2020-04-19 23:20:16 +0900

    added nodemon, multer and set up post route for file upload

--- server.js
+++ server.js
@@ -2,22 +2,25 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+const multer = require('multer');
 
 var app = express();
-
 app.use(cors());
+
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.get('/hello', function (req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.post('/api/fileanalyse', (req, res)=> {
+  console.log('yo')
+});
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
